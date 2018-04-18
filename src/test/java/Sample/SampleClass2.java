4dd61c3c33504b5e2aa7ff8374e40c6649a147e6
Krishna Venkumahanthi <Krishna.Venkumahanthi@macys.com>
2018-04-18 16:01:44 +0530

    CBT Changes

--- src/test/java/Sample/SampleClass2.java
+++ src/test/java/Sample/SampleClass2.java
@@ -1,5 +1,7 @@
 package Sample;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -15,7 +17,9 @@ public class SampleClass2 {
 		//WebDriver dr= new FirefoxDriver();
 		 
 			dr.navigate().to("http://www.rediff.com/");
+			dr.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 			dr.manage().window().maximize();
+			//Wait for 2 milliseconds
 			Thread.sleep(2000);
 			dr.quit();
 
